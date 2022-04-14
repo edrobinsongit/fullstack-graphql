1b14ffa7836d1be7a523cf2b8df3bac2f5bc1407
Ed Robinson <edrobinson@icloud.com>
2022-04-14 18:43:35 +0100

    End of course

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,29 @@
 
 module.exports = {
   Query: {
-    
+    pets: (_, { input }, context) => { console.log(JSON.stringify(input, null, 2)); console.log(JSON.stringify(context, null, 2)); return context.models.Pet.findMany(input); },
+    pet: (_, { input }, context) => { console.log(JSON.stringify(input, null, 2)); console.log(JSON.stringify(context, null, 2)); return context.models.Pet.findOne(input); }
   },
   Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    newPet: (_, { input }, context) => {
+      return context.models.Pet.create(input)
     }
   },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
   User: {
-    
+    pets: (_, __, context) => {
+      return context.models.Pet.findMany()
+    }
+  },
+  Pet: {
+    owner: (_, __, context) => {
+      return context.models.User.findOne();
+    }
   }
 }
